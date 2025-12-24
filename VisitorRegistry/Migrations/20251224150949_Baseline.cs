4bceaef9e251655fbe7ea0cb321957fc4b0d19ec
giuliavanni <giulia.vanni6@studio.unibo.it>
2025-12-24 16:31:32 +0100

    baseline update

--- VisitorRegistry/Migrations/20251224150949_Baseline.cs
+++ VisitorRegistry/Migrations/20251224150949_Baseline.cs
@@ -1,19 +1,22 @@
-using Microsoft.EntityFrameworkCore.Migrations;
+﻿using Microsoft.EntityFrameworkCore.Migrations;
 
 #nullable disable
 
 namespace VisitorRegistry.Migrations
 {
+    /// <inheritdoc />
     public partial class Baseline : Migration
     {
+        /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            // intentionally empty (baseline migration)
+
         }
 
+        /// <inheritdoc />
         protected override void Down(MigrationBuilder migrationBuilder)
         {
-            // intentionally empty
+
         }
     }
 }
