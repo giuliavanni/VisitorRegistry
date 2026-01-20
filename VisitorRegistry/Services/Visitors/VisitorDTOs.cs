6e3373f4aa5b085bea264fa7cb795ba990474d67
assmaamatouk <assmaa.matouk@studio.unibo.it>
2026-01-20 06:40:30 +0100

    Update VisitorDTOs.cs

--- VisitorRegistry/Services/Visitors/VisitorDTOs.cs
+++ VisitorRegistry/Services/Visitors/VisitorDTOs.cs
@@ -1,21 +1,23 @@
-﻿using System;
+using System;
 
 namespace VisitorRegistry.Services.Visitors
 {
     public class VisitorCreateDTO
     {
-        public string Nome { get; set; }
-        public string Cognome { get; set; }
-        public DateTime DataVisita { get; set; }
-        public string QrCode { get; set; }
+        public string Nome { get; set; } = "";
+        public string Cognome { get; set; } = "";
+        public DateTime? DataVisita { get; set; } = DateTime.Now;
+        public string? QrCode { get; set; }
+        public DateTime? CheckIn { get; set; }
+        public DateTime? CheckOut { get; set; }
     }
 
     public class VisitorUpdateDTO
     {
-        public string Nome { get; set; }
-        public string Cognome { get; set; }
-        public DateTime DataVisita { get; set; }
-        public string QrCode { get; set; }
+        public string Nome { get; set; } = "";
+        public string Cognome { get; set; } = "";
+        public DateTime? DataVisita { get; set; }
+        public string? QrCode { get; set; }
     }
 
     public class VisitorDetailDTO
@@ -42,4 +44,18 @@ namespace VisitorRegistry.Services.Visitors
         public int? CurrentPresenceId { get; set; }   //id della visita mostrata nella riga
         public string QrCode { get; set; }
     }
+    public class VisitorEditDTO
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Cognome { get; set; }
+        public string Ditta { get; set; }
+        public string Referente { get; set; }
+        public DateTime DataVisita { get; set; }
+        public DateTime? CheckInTime { get; set; }
+        public DateTime? CheckOutTime { get; set; }
+        public string QrCode { get; set; } // rimane invariato
+    }
+
+
 }
